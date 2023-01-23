de458b3571de30c28697fe3448d5af7a0d138d70
Tatiana Levina <tatiana.levina.v@gmail.com>
2023-01-24 00:59:19 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let history = new Array();
+  for (let i = 0; i < str.length; i++) {
+    for (let bracketType = 0; bracketType < bracketsConfig.length; bracketType++) {
+      if (bracketsConfig[bracketType][0] === bracketsConfig[bracketType][1]) {
+        if (str[i] === bracketsConfig[bracketType][0]){
+          if (history.length === 0 || history.at(-1)[0] !== bracketType){
+            history.push([bracketType, 1]);
+          } else {
+            history.pop();
+          } 
+          break;
+        }  
+      } else if (str[i] === bracketsConfig[bracketType][0]) {
+        if (history.length === 0 || history.at(-1)[0] !== bracketType) {
+          history.push([bracketType, 1]);
+        } else {
+          history.at(-1)[1] += 1;
+        }
+        break;
+      } else if (str[i] === bracketsConfig[bracketType][1]) {
+        if (history.length === 0 || history.at(-1)[0] !== bracketType) {
+          return false;
+        }
+        history.at(-1)[1] -= 1;
+        if (history.at(-1)[1] === 0) {
+          history.pop();
+        }
+        break;
+      }
+    }
+  }
+  return history.length === 0;
 }
